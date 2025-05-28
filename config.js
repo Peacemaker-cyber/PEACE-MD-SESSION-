38d928353b5fef8371424ce8e5d1932d3ddc93c2
Peacemaker Tech <muuoemmanuel649@gmail.com>
2025-05-28 13:07:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  PREFIX: process.env.PREFIX || "JUNE-MD~",
-  EMAIL: process.env.EMAIL || "tylorxxxxxxx@gmail.com",
-  PASS: process.env.PASS || "tylorxxxxx",
-  MESSAGE: process.env.MESSAGE || "*JUNE-MD SESSION BELOW*",
-  IMAGE: process.env.IMAGE || "https://files.catbox.moe/rfc3zo.jpg",
+  PREFIX: process.env.PREFIX || "PEACE-MD~",
+  EMAIL: process.env.EMAIL || "muuoxxxxxxx@gmail.com",
+  PASS: process.env.PASS || "muuoxxxxx",
+  MESSAGE: process.env.MESSAGE || "PEACE-MD SESSION BELOW*",
+  IMAGE: process.env.IMAGE || "https://files.catbox.moe/yndl4u.jpg",
 }
